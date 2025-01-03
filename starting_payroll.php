71a06cd892ca2afbe5242be164f7884b360e8de0
Ejxzdevs <ejhayyygofredo08@gmail.com>
2025-01-03 14:43:52 +0800

    convert if else into switch

--- starting_payroll.php
+++ starting_payroll.php
@@ -1,19 +1,20 @@
-<?php 
+<?php
 include "connection/connection.php";
 
+$select = "SELECT COUNT(*) FROM tbl_accounts";
+$query = mysqli_query($conn, $select);
 
-
-$select = "SELECT * FROM tbl_accounts ";
-$query = mysqli_query($conn,$select);
-$row = mysqli_num_rows($query);
-
-
-if($row == 0){
-	header("Location: user-entry/configure_company.php");
-}else{
-	header("Location: user-entry/login.php");
+if (!$query) {
+    die("Query failed: " . mysqli_error($conn));
 }
 
+$row = mysqli_fetch_array($query);
 
-
-?>
+switch ($row[0]) {
+    case 0:
+        header("Location: user-entry/configure_company.php");
+        exit();
+    default:
+        header("Location: user-entry/login.php");
+        exit();
+}
