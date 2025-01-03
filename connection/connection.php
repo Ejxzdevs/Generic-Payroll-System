20b696f0ecac571343bc46dbd8b09606ede1607a
Ejxzdevs <ejhayyygofredo08@gmail.com>
2025-01-03 14:45:55 +0800

    add error handler

--- connection/connection.php
+++ connection/connection.php
@@ -1,10 +1,12 @@
 <?php
-	$server = "localhost";
-	$username = "root";
-	$password = "12345";
-	$db = "payroll_system";
+$server = "localhost";
+$username = "root";
+$password = "";
+$db = "payroll_system";
 
+$conn = new mysqli($server, $username, $password, $db);
 
-	$conn = new mysqli($server,$username,$password,$db);
-
-?>
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
